ff683e1a8e009e116434d089b4bca3a8613456d8
草鞋没号 <308487730@qq.com>
2022-03-09 10:05:41 +0800

    add axios

---
+++ utils/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { notification } from 'antd';
+
+const axiosInstance = axios.create({
+  // 这里写一些请求固定参数
+});
+
+axiosInstance.interceptors.response.use(
+  response => {
+    // 这里加工下返回的数据格式；如果返回数据错误信息也可以在这里抛出提示
+    // notification.error(response.data.message);
+  },
+  error => {
+    // 这里写请求错误处理
+  },
+);
+
+export { axiosInstance as default }
