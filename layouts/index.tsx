88d8ff1818c4a79e987b5310794c9fcc796eefc0
草鞋没号 <308487730@qq.com>
2022-03-09 09:58:32 +0800

    feat: 基本布局

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
 import { Link, Outlet } from 'umi';
-import styles from './index.less';
+import { Layout, Menu } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+import './index.less';
 
-export default function Layout() {
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+
+export default function AppLayout() {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
+    <Layout className="app-layout">
+      <Header className="header">
+        <div className="logo" />
+      </Header>
+      <Layout className="layout-side">
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <Menu.Item key="1">
+              <Link to="/">首页</Link>
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<MenuOutlined />} title="菜单组">
+              <Menu.Item key="2">
+                <Link to="/docs">文档</Link>
+              </Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
