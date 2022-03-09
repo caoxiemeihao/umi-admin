eac91b0f0e6c85ad083a7b20d4e2563604a7468d
草鞋没号 <308487730@qq.com>
2022-03-09 10:14:47 +0800

    axios demo

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from '@/utils/api';
 
 export default () => {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/9')
+      .then(result => {
+        setData(JSON.stringify(result as any, null, 2));
+      });
+  }, []);
+
   return (
     <div>
       <p>This is umi docs.</p>
+      <hr />
+      <div>
+        <pre dangerouslySetInnerHTML={{ __html: data }} />
+      </div>
     </div>
   );
 };
--- utils/api.ts
+++ utils/api.ts
@@ -9,6 +9,7 @@ axiosInstance.interceptors.response.use(
   response => {
     // 这里加工下返回的数据格式；如果返回数据错误信息也可以在这里抛出提示
     // notification.error(response.data.message);
+    return response.data;
   },
   error => {
     // 这里写请求错误处理
