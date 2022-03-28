f59004597bcdd2e7ed3c181ef5b511a38bcbc83d
草鞋没号 <308487730@qq.com>
2022-03-28 19:09:28 +0800

    feat: 登录拦截

--- app.tsx
+++ app.tsx
@@ -1,1 +1,11 @@
+import { history } from 'umi'
+import { getUserInfo } from '@/utils/auth'
 import './styles/bootstrap-partial.less'
+
+export function onRouteChange({ location, routes, action }) {
+  const userInfo = getUserInfo()
+  if (!userInfo && location.pathname !== '/login') {
+    // 登录校验
+    history.push('/login')
+  }
+}
